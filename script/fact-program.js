3cf79a614ee4e7612d9f7fe09a7556abd7264549
Thomás Borges <thomasbrgsva@gmail.com>
2021-08-15 23:47:03 -0300

    Debug calculate factorial condition

--- script/fact-program.js
+++ script/fact-program.js
@@ -14,7 +14,7 @@ function addValuesFact(input_number, printed_values) {
 
 function calculateFactorials(result_box) {
 
-    if (imputedFactValues = []) return;
+    if (imputedFactValues == []) return;
 
     let calculatedFactorials = [];
 
