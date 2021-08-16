c972e9f172fef6bd81417f539cc809f1f5ccd357
Thomás Borges <thomasbrgsva@gmail.com>
2021-08-15 22:34:33 -0300

    Update opacity dinamic behavior

--- script/general.js
+++ script/general.js
@@ -5,37 +5,31 @@ let factorial_box = document.querySelector('section#factorial-box');
 multiples_box.addEventListener('mouseover', function() {
     factorial_box.style.opacity = 0.3;
     mmc_box.style.opacity = 0.3;
-    multiples_box.style.opacity = 1;
 })
 
 multiples_box.addEventListener('mouseout', function() {
-    factorial_box.style.opacity = 0.7;
-    mmc_box.style.opacity = 0.7;
-    multiples_box.style.opacity = 0.7;
+    factorial_box.style.opacity = 1;
+    mmc_box.style.opacity = 1;
 })
 
 mmc_box.addEventListener('mouseover', function() {
     multiples_box.style.opacity = 0.3;
     factorial_box.style.opacity = 0.3;
-    mmc_box.style.opacity = 1;
 })
 
 mmc_box.addEventListener('mouseout', function() {
-    multiples_box.style.opacity = 0.7;
-    factorial_box.style.opacity = 0.7;
-    mmc_box.style.opacity = 0.7;
+    multiples_box.style.opacity = 1;
+    factorial_box.style.opacity = 1;
 })
 
 factorial_box.addEventListener('mouseover', function() {
     multiples_box.style.opacity = 0.3;
     mmc_box.style.opacity = 0.3;
-    factorial_box.style.opacity = 1;
 })
 
 factorial_box.addEventListener('mouseout', function() {
-    multiples_box.style.opacity = 0.7;
-    mmc_box.style.opacity = 0.7;
-    factorial_box.style.opacity = 0.7;
+    multiples_box.style.opacity = 1;
+    mmc_box.style.opacity = 1;
 })
 
 function copyContent(text) {
